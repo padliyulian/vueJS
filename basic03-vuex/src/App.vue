6e2d8b49245106ea7079591f94fb63f0bebff289
padli yulian <padliyulian@ymail.com>
2018-12-25 21:41:50 +0700

    second

---
+++ basic03-vuex/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <product-list-one></product-list-one>
+    <product-list-two></product-list-two>
+  </div>
+</template>
+
+<script>
+import productListOne from "./components/productListOne.vue";
+import productListTwo from "./components/productListTwo.vue";
+
+export default {
+  components: {
+    "product-list-one": productListOne,
+    "product-list-two": productListTwo
+  },
+  name: "app",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="sass">
+</style>
