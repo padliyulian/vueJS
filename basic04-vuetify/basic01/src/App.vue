f7786efa86fbd7c99e60cff74948df066f30d600
padli yulian <padliyulian@ymail.com>
2019-01-01 13:46:28 +0700

    vue

---
+++ basic04-vuetify/basic01/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <navbar/>
+    <v-content class="ma-4">
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import navbar from "@/components/Navbar";
+
+export default {
+  components: {
+    navbar
+  },
+  data() {
+    return {
+      //
+    };
+  }
+};
+</script>
