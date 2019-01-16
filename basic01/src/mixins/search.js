715ae924bff003b4c4254150d1f4a24e9f8d6740
padliyulian <padliyulian@ymail.com>
2019-01-16 16:45:19 +0700

    clean

--- basic01/src/mixins/search.js
+++
@@ -1,9 +0,0 @@
-export default {
-  computed: {
-    searchPost: function () {
-      return this.posts.filter(post => {
-        return post.title.toLowerCase().match(this.search.toLowerCase());
-      });
-    }
-  }
-}
